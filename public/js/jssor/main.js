c8b3b84fe729dd87427b74c7f842cc05fbcd9281
ubuntu12_lab <ubuntu12_924@ubuntu.com>
2015-08-07 07:18:53 +0800

    20150807

--- public/js/jssor/main.js
+++ public/js/jssor/main.js
@@ -153,7 +153,7 @@ jQuery(document).ready(function ($) {
     $BulletNavigatorOptions: {                //[Optional] Options to specify and enable navigator or not
       $Class: $JssorBulletNavigator$,             //[Required] Class to create navigator instance
       $ChanceToShow: 2,                 //[Required] 0 Never, 1 Mouse Over, 2 Always
-      $AutoCenter: 1,                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+      $AutoCenter: 0,                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
       $Steps: 1,                    //[Optional] Steps to go for each navigation request, default value is 1
       $Lanes: 1,                    //[Optional] Specify lanes to arrange items, default value is 1
       $SpacingX: 4,                   //[Optional] Horizontal space between each item in pixel, default value is 0
