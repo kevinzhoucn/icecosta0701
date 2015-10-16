4270b0ee6275b1490091daf93a98f85790ff1d3f
ubuntu12_lab <ubuntu12_924@ubuntu.com>
2015-10-16 20:16:49 +0800

    20151016 evening

--- app/controllers/cpanel/application_controller.rb
+++ app/controllers/cpanel/application_controller.rb
@@ -2,5 +2,9 @@ class Cpanel::ApplicationController < ActionController::Base
   layout 'cpanel'
   protect_from_forgery with: :exception
   
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :set_params
+
+  def set_params
+    @site_maintenace_notice = SiteConfig.get_config('maintenance_notice')
+  end
 end
