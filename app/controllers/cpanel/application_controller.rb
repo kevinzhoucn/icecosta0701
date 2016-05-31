1472fdb210093df941790e790a31c9a5d12d053e
ubuntu12_lab <ubuntu12_924@ubuntu.com>
2016-05-31 21:57:03 +0800

    20160601 紧急修复：之前这句被注释掉了before_filter ：authenticate_user！， ：set_params，导致后台不需要登录

--- app/controllers/cpanel/application_controller.rb
+++ app/controllers/cpanel/application_controller.rb
@@ -2,7 +2,7 @@ class Cpanel::ApplicationController < ActionController::Base
   layout 'cpanel'
   protect_from_forgery with: :exception
   
-  #before_filter :authenticate_user!, :set_params
+  before_filter :authenticate_user!, :set_params
 
   def set_params
     @site_maintenace_notice = SiteConfig.get_config('maintenance_notice')
